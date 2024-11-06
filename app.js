40cfa0828abb0b8d45de1c624b8c375a2b45c693
Nihalkarimbil <muhammednihalk4@gmail.com>
2024-11-06 11:49:28 +0530

    cors updates

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
-const express=require('express')
-const mongoose=require('mongoose')
-const userRout=require('./routes/userRoutes')
-const adminRoute=require('./routes/adminRoutes')
+const express = require('express')
+const mongoose = require('mongoose')
+const userRout = require('./routes/userRoutes')
+const adminRoute = require('./routes/adminRoutes')
 // const stripeRoute=require('./routes/stripeRoute')
-const errorhandler=require('./middleware/erorhandler')
-const CookieParser=require('cookie-parser')
-const cors =require('cors')
+const errorhandler = require('./middleware/erorhandler')
+const CookieParser = require('cookie-parser')
+const cors = require('cors')
 
-const app= express()
+const app = express()
 app.use(express.json())
 app.use(CookieParser())
 
@@ -16,27 +16,25 @@ app.use(CookieParser())
 app.use(cors({
     origin: "https://furniture-ecommerce-app-three.vercel.app/",
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  }))
+}))
 
-app.use('/api',userRout)
-app.use('/api',adminRoute)
+app.use('/api', userRout)
+app.use('/api', adminRoute)
 // app.use('/api', stripeRoute);
 
 app.use(errorhandler)
 
-mongoose.connect(process.env.MONGO_URI) 
-.then(()=>{
-    console.log('connected to database')
-})
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('connected to database')
+    })
 
-.catch((error)=>{
-    console.log('database connection error',error)
-})
+    .catch((error) => {
+        console.log('database connection error', error)
+    })
 
 
 
-app.listen(5001,()=>{
+app.listen(5001, () => {
     console.log('server running on port 5001');
 })
